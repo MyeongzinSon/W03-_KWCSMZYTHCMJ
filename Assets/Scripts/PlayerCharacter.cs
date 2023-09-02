308c575306b6a4b1a9e8034ed28ddc4347a89f65
손명진 <sonmyungzin1@gmail.com>
2023-09-02 14:49:06 +0900

    (+) 플레이어 캐릭터와 고스트 캐릭터 예제 코드 추가

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,11 +5,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerCharacter : CharacterBase, PlayerInputActions.IPlayerActions
 {
+    [SerializeField] InputRecorder recorder;
+    [SerializeField] InputDecoder decoder;
+    [SerializeField] GhostCharacter decodeTarget;
+
     private void Awake()
     {
         PlayerInputActions inputs = new();
         inputs.Player.SetCallbacks(this);
         inputs.Enable();
+
     }
 
     public void OnInteract(InputAction.CallbackContext context)
@@ -17,17 +22,49 @@ public class PlayerCharacter : CharacterBase, PlayerInputActions.IPlayerActions
         if (context.performed)
         {
             Interact();
+            if (recorder.IsRecording)
+            {
+                recorder.RecordCurrentInput(true);
+            }
         }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         inputDirection = context.ReadValue<Vector2>();
+        if (recorder.IsRecording)
+        {
+            recorder.RecordCurrentInput(inputDirection);
+        }
     }
 
     void Update()
     {
-        Debug.Log($"Inputs = {inputDirection}");
+        transform.Translate(3f * inputDirection * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartRecord();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            EndRecordAndStartDecode();
+        }
+    }
+
+    void StartRecord()
+    {
+        recorder.StartRecord();
+    }
+
+    void EndRecordAndStartDecode()
+    {
+        recorder.EndRecord();
+
+        recorder.TryGetInputQueue(out var recordedQueue);
+
+        decoder.DecodeInputQueue(recordedQueue);
+        decoder.StartDecode(decodeTarget);
     }
 
     void Interact()
