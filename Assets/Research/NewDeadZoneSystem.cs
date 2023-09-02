b2198e0a8809649a20fd4ad61058c58f557d8368
YuTaehwan <ytw0113@gmail.com>
2023-09-02 14:50:13 +0900

    working on player research

--- Assets/Research/NewDeadZoneSystem.cs
+++ Assets/Research/NewDeadZoneSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewDeadZoneSystem : MonoBehaviour
@@ -10,30 +11,55 @@ public class NewDeadZoneSystem : MonoBehaviour
     private GameObject _deadZone;
 
     [SerializeField] private bool _isAuto;
+    private bool _isCollidingWithPlayer;
+    private NewPlayerSystem _targetPlayer;
     
     //Awake
-    private void Awake()
-    {
+    private void Awake() {
         //Find PlayerDetect GameObject
         _playerDetect = transform.Find("PlayerDetect").gameObject;
         //Find DeadZone GameObject
         _deadZone = transform.Find("DeadZone").gameObject;
+
+        if (_isAuto) {
+            _playerDetect.SetActive(true);
+            _deadZone.SetActive(true);
+        } else {
+            _playerDetect.SetActive(false);
+            _deadZone.SetActive(true);
+        }
+    }
+
+    private void Update() {
+        
     }
     
-    //OnTriggerEnter2D
-    private void OnTriggerExit2D(Collider2D other)
+    //OnTriggerStay2D
+    private void OnTriggerStay2D(Collider2D other)
     {
+        NewPlayerSystem player = other.GetComponent<NewPlayerSystem>();
         //If other is Player Tag
-        if (other.CompareTag("Player"))
-        {
-            //Start SetActiveTrue() Coroutine
-            StartCoroutine(SetActiveTrue());
+        if (player != null) {
+            if (_isAuto) {
+                _playerDetect.SetActive(true);
+                _deadZone.SetActive(false);
+            }
+            else {
+                player.
+            }
+            _isCollidingWithPlayer = true;
+            _targetPlayer = player;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            _isCollidingWithPlayer = false;
         }
     }
     
     //SetActive(true) Coroutine
-    public IEnumerator SetActiveTrue()
-    {
+    public IEnumerator SetActiveTrue() {
         //Wait for 1 second
         yield return new WaitForSeconds(2f);
         
